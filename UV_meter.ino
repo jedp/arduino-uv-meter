ccb8fe12e151330fcfed09eb0289ef1295a43444
Jed Parsons <jedp@me.com>
2014-07-24 12:27:05 -0700

    Added piezo speaker chirp; refactored

--- UV_meter.ino
+++ UV_meter.ino
@@ -21,6 +21,8 @@
   - TSL2561 SCL pin to Analog pin 5
   - TSL2561 SDA pin to Analog pin 4
 
+  - PWM pin A1
+
  The two I2C boards (Si1145 and TSL2561) share the I2C clock and data on analog
  pins 4 and 5.
 
@@ -44,12 +46,22 @@ Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 1234
 float uvIndex;
 sensors_event_t event;
 
-const int S12SD_PIN = 0;
+const int S12SD_PIN = 0;    // analog
+
+const int PWM_PIN = 3;      // digital PWM
+const long CHIRP_DURATION = 80000;
+const int CHIRP_PITCH = 800;
+
+const float NORMAL_UV = 8.0;
+const float SAMPLE_RATE = 1000;
 
 // RS, E, D4 .. D7
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 void setup() {
+  pinMode(PWM_PIN, OUTPUT);
+  chirp();
+
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
 
@@ -75,53 +87,77 @@ void setup() {
   tsl.enableAutoRange(true);
   tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);
 
+  delay(2000);
+
   lcd.clear();
 }
 
-void loop() {
+void chirp() {
+  // Chirp the piezo speaker using one of the PWM digital outs
+  long t = 0;
+  while (t < CHIRP_DURATION) {
+    digitalWrite(PWM_PIN, HIGH);
+    delayMicroseconds(CHIRP_PITCH / 2);
+    digitalWrite(PWM_PIN, LOW);
+    delayMicroseconds(CHIRP_PITCH / 2);
+    t += CHIRP_PITCH;
+  }
+}
+
+void printUVIndex(float uvIndex) {
   lcd.home();
-
-  uvIndex = uv.readUV() / 100.0;
-
   lcd.print("UV: ");
   lcd.print(uvIndex);
   lcd.print("        ");
 
-  // Analog UV reading
-  lcd.setCursor(12, 0);
-  lcd.print(analogRead(S12SD_PIN));
-  lcd.print("   ");
-
-  // Beginning of second line
-  lcd.setCursor(0, 1);
+  lcd.setCursor(10, 0);
 
   // The EPA UV Index, http://www2.epa.gov/sunwise/uv-index-scale
   if (uvIndex <= 2.0) {
-    lcd.print("Low     ");
+    lcd.print("Low   ");
   }
   else if (uvIndex <= 5.0) {
-    lcd.print("Mod     ");
+    lcd.print("Mod   ");
   }
   else if (uvIndex <= 7.0) {
-    lcd.print("High    ");
+    lcd.print("High  ");
   }
   else if (uvIndex <= 10) {
-    lcd.print("V High  ");
+    lcd.print("V High");
   }
   else {
-    lcd.print("Yikes   ");
+    lcd.print("Yikes ");
   }
+}
 
-  // Lux reading
+void getAndPrintAnalogUV() {
+  lcd.setCursor(0, 1);
+  lcd.print(analogRead(S12SD_PIN));
+  lcd.print("       ");
+}
+
+void getAndPrintLux() {
   tsl.getEvent(&event);
-  if (event.light) {
-    lcd.print((int)(event.light));
-    lcd.print(" lux");
+  lcd.setCursor(8, 1);
+  long lux = (long)(event.light);
+  if (lux) {
+    lcd.print(lux);
+    lcd.print(" lx");
   } else {
-    lcd.print("oops");
+    lcd.print("much");
   }
   lcd.print("        ");
+}
 
-  delay(1000);
+void loop() {
+  uvIndex = uv.readUV() / 100.0;
+
+  printUVIndex(uvIndex);
+  getAndPrintAnalogUV();
+  getAndPrintLux();
+
+  //chirp();
+
+  delay(SAMPLE_RATE);
 }
 
